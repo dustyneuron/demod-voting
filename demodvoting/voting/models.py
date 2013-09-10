7d673a2b80c3a89092d741f0562775746710d374
Tom Scholl <tom@dustyneuron.com>
2013-09-10 19:21:32 +0100

    Work on AV tie-break (broken)

--- demodvoting/voting/models.py
+++ demodvoting/voting/models.py
@@ -27,10 +27,61 @@ def find_winners_av():
             final_winners[change] = num_votes
         
     return list(final_winners.items())
-        
+    
 
 class Section(models.Model):
     name = models.CharField(max_length=200)
+
+    def _av_tie_break(self, tuple_list, find_worst=False):
+        if len(tuple_list) == 0:
+            raise Exception('av tie break given empty list')
+        
+        all_pris = set([])
+        dic = {}
+        for change, av_data in tuple_list:
+            counts = {}
+            for v in Vote.objects.filter(section=self, change=change).all():
+                if v.priority not in counts:
+                    counts[v.priority] = 0
+                counts[v.priority] += 1
+            
+            all_pris = all_pris.union(counts.keys())
+            for p, n in counts.items():
+                if p not in dic:
+                    dic[p] = {'data':{}, 'nums':set([])}
+                if n not in dic[p]['data']:
+                    dic[p]['data'][n] = []
+                    
+                dic[p]['data'][n].append((change, av_data))
+                dic[p]['nums'].add(n)
+        
+        all_pris = list(all_pris)    
+        all_pris.sort()
+                
+        group = []
+        all_best_first = []
+        for p in all_pris:
+            dic[p]['nums'] = list(dic[p]['nums'])
+            dic[p]['nums'].sort()
+            dic[p]['nums'].reverse()
+                
+            for n in dic[p]['nums']:
+                group = dic[p]['data'][n]
+                if find_worst:
+                    all_best_first.append(group)
+                elif len(group) == 1:
+                    return group[0]
+        
+        if find_worst:
+            group = all_best_first.pop()
+        
+        if len(group) == 1:
+            return group[0]
+        elif len(group) > 1:
+            # TODO: random
+            return group[0]
+        raise Exception('AV tie break failed')
+        
 
     # Alternative vote implementation, TODO: optimize
     @transaction.commit_on_success
@@ -44,22 +95,28 @@ class Section(models.Model):
 
         while True:                
             lowest_count, lowest_c = None, None
-            for c in first_votes.keys():
-                if first_votes[c] >= win_threshold:
-                    return (c, first_votes[c])
-                    
-                if (lowest_count == None) or (first_votes[c] < lowest_count):
-                    lowest_count = first_votes[c]
-                    lowest_c = c
+            tuple_list = list(first_votes.items())
+            tuple_list.sort(key=lambda x: x[1])
             
-            # eliminate lowest_c
+            highest_count = tuple_list[len(tuple_list)-1][1]
+            if highest_count >= win_threshold:
+                top = [t for t in tuple_list if t[1] == highest_count]
+                return self._av_tie_break(top)
+            
+            lowest_count = tuple_list[0][1]
+            elim_cands = [t for t in tuple_list if t[1] == lowest_count]
+            lowest_c, _ = self._av_tie_break(elim_cands, find_worst=True)
+                        
+            # re-assign votes for lowest_c to next pref
             for orig_vote in Vote.objects.filter(section=self, change=lowest_c):
                 alternates = Vote.objects.filter(user=orig_vote.user, section=self, priority__gt=orig_vote.priority)
                 if len(alternates) > 0:
-                    if alternates[0].change not in first_votes:
-                        first_votes[alternates[0].change] = 0
-                    first_votes[alternates[0].change] += 1
+                    alt = alternates[0].change
+                    if alt not in first_votes:
+                        first_votes[alt] = 0
+                    first_votes[alt] += 1
             
+            # eliminate lowest_c
             del first_votes[lowest_c]
             
             if len(first_votes) == 0:
