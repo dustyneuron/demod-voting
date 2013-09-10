82c6bd65e4ac9c35d6cec12053cd077d77a43769
Tom Scholl <tom@dustyneuron.com>
2013-09-10 17:16:39 +0100

    Added failing AV test case

--- demodvoting/voting/tests.py
+++ demodvoting/voting/tests.py
@@ -4,20 +4,26 @@ from voting.models import *
 from django.contrib.auth.models import User
 
 class AVTest(TestCase):
+    def users(self, count):
+        results = []
+        for i in range(count):
+            u = User(username='user' + str(i))
+            u.save()
+            results.append(u)
+        return tuple(results)
+        
+    def sections(self, count):
+        results = []
+        for i in range(count):
+            u = Section(name='section' + str(i))
+            u.save()
+            results.append(u)
+        return tuple(results)
+    
     def test_av_simple(self):
-        fred = User(username='fred')
-        fred.save()
-        bob = User(username='bob')
-        bob.save()
-        dave = User(username='dave')
-        dave.save()
-
-        core_section = Section(name='core_code')
-        core_section.save()
-        util_section = Section(name='util_code')
-        util_section.save()
-        html_section = Section(name='html_code')
-        html_section.save()
+        u1, u2, u3 = self.users(3)
+        
+        core_section, util_section, html_section = self.sections(3)
 
         html_fix1 = Change(name='html_fix1')
         html_fix1.save()
@@ -31,11 +37,35 @@ class AVTest(TestCase):
         massive_fix.save()
         massive_fix.sections.add(core_section, util_section, html_section)
 
-        set_user_votes(fred, html_section, [html_fix1])
-        set_user_votes(bob, html_section, [html_fix2, massive_fix])
-        set_user_votes(dave, html_section, [massive_fix])
+        set_user_votes(u1, html_section, [html_fix1])
+        set_user_votes(u2, html_section, [html_fix2, massive_fix])
+        set_user_votes(u3, html_section, [massive_fix])
                 
         winners = find_winners_av()
         self.assertEqual(len(winners), 1)
         change, num_votes = winners[0]
         self.assertEqual((change.name, num_votes), ('massive_fix', 2))
+
+    def test_av_two_competing_sections(self):
+        u1, u2, u3, u4, u5 = self.users(5)
+        s1, s2 = self.sections(2)
+
+        fix1 = Change(name='fix1')
+        fix1.save()
+        fix1.sections.add(s1, s2)
+        
+        fix2 = Change(name='fix2')
+        fix2.save()
+        fix2.sections.add(s1, s2)
+
+        for u in [u1, u2, u3, u4]:
+            set_user_votes(u, s1, [fix1])
+            set_user_votes(u, s2, [fix2])
+
+        set_user_votes(u5, s1, [fix1, fix2])
+        set_user_votes(u5, s2, [fix1, fix2])
+                
+        winners = find_winners_av()
+        self.assertEqual(len(winners), 1)
+        change, num_votes = winners[0]
+        self.assertEqual((change.name, num_votes), ('fix1', 5))
